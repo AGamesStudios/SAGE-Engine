0543d38e248255701838c2f4a73255ec60832875
AGamesStudios <amckinatorgamesstudios@gmail.com>
2025-06-24 07:57:17 +0000

    Add unified package and fix collision

---
+++ sage/editor.py
@@ -0,0 +1,5 @@
+"""Entry point proxy for the editor package."""
+from sage_editor.__main__ import main
+
+if __name__ == '__main__':
+    main()
---
+++ sage/engine.py
@@ -0,0 +1,5 @@
+"""Entry point proxy for the engine package."""
+from engine.__main__ import main
+
+if __name__ == '__main__':
+    main()
